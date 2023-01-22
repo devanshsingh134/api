ee644f81fc07dc6057c84b244a821ae41eb573ab
devanshsingh134 <70709245+devanshsingh134@users.noreply.github.com>
2023-01-22 20:44:40 +0530

    Update views.py

--- API/views.py
+++ API/views.py
@@ -9,4 +9,76 @@ class MemberViewset(viewsets.ModelViewSet):
     serializer_class= MemberSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+   
+
+# ****************************************************************************************************************************
+'''
+the  following commented code also work same as above...
+
+'''
+# from django.shortcuts import render
+# from .serializers import StudentSerializer
+# from .models import Student
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+#
+#
+#
+#
+# # Create your views here.
+#
+# @api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+# def StudentAPI(request, pk):
+#     if request.method=='GET':
+#
+#
+#         if id is not None:
+#             stu = Member.objects.get(pk=pk)
+#             serializer = MemberSerializer(stu)
+#             return Response(serializer)
+#     stu = Member.objects.all()
+#     serializer = MemberSerializer(stu, many=True)
+#     json_data = JSONRenderer().render(serializer.data)
+#     return HttpResponse(json_data, content_type="application/json")
+#
+# def post(self, request, *args, **kwargs):
+#     json_data = request.body
+#     stream = io.BytesIO(json_data)
+#     pythondata = JSONParser().parse(stream)
+#     serializer = MemberSerializer(data=pythondata)
+#     if serializer.is_valid():
+#         serializer.save()
+#         res = {'msg': 'data saved'}
+#         json_data = JSONRenderer().render(res)
+#         return HttpResponse(json_data, content_type='application/json')
+#     json_data = JSONRenderer().render(serializer.errors)
+#     return HttpResponse(json_data, content_type="application/json")
+#
+# def put(self, request, *args, **kwargs):
+#     json_data = request.body
+#     stream = io.BytesIO(json_data)
+#     pythondata = JSONParser().parse(stream)
+#     id = pythondata.get('id')
+#     stud = Member.objects.get(id=id)
+#     serializer = MemberSerializer(stud, data=pythondata, partial=True)
+#     if serializer.is_valid():
+#         serializer.save()
+#         res = {'msg': "updated"}
+#         json_data = JSONRenderer().render(res)
+#         return HttpResponse(json_data, content_type='application/json')
+#     json_data = JSONRenderer().render(serializer.errors)
+#     return HttpResponse(json_data, content_type='application/json')
+#
+# def delete(self, request, *args, **kwargs):
+#     json_data = request.body
+#     stream = io.BytesIO(json_data)
+#     pythondata = JSONParser().parse(stream)
+#     id = pythondata.get('id')
+#     stud = Member.objects.get(id=id)
+#     stud.delete()
+#     res = {"msg": "deleted"}
+#     json_data = JSONRenderer().render(res)
+#     return HttpResponse(json_data, content_type='application/json')
+
+
 
